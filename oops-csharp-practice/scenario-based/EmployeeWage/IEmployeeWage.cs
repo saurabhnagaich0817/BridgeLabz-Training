8a6bd4897c60e269cac798ca900bf47106b38849
saurabhnagaich0817 <saurabhnagaich27@gmail.com>
2026-01-07 15:15:30 +0530

    [saurabh]UC1: Added employee wage interface

---
+++ oops-csharp-practice/scenario-based/EmployeeWage/IEmployeeWage.cs
@@ -0,0 +1,14 @@
+interface IEmployeeWage
+{    
+
+  // this is for UC-1
+    void CheckAttendance();  
+    // this is for UC-2
+    void CalculateDailyWage();       
+    // this is for UC-3
+    void PartTimeWage();             
+    // this is for UC-5
+    void MonthlyWage();              
+    // this is for UC-6
+    void WageTillCondition();        
+}
