29ddcfbdd486b672af04f05da27978a39a47bc93
saurabhnagaich0817 <saurabhnagaich27@gmail.com>
2026-01-15 12:05:54 +0530

    UC5: Add multiple contacts using array with interface

--- oops-csharp-practice/scenario-based/AddressBook/AddressBookUtilityImpl.cs
+++ oops-csharp-practice/scenario-based/AddressBook/AddressBookUtilityImpl.cs
@@ -1,123 +1,120 @@
 using System;
 
-public class AddressBookUtilityImpl
+public class AddressBookUtilityImpl : IAddressBook
 {
-    private Contact contact = new Contact();
-    private bool isContactAdded = false;
+    private Contact[] contacts = new Contact[10];
+    private int count = 0;
 
-    // UC-1 : Add Contact
+  
     public void AddContact()
     {
-        Console.Write("Enter First Name: ");
-        contact.SetFirstName(Console.ReadLine());
-
-        Console.Write("Enter Last Name: ");
-        contact.SetLastName(Console.ReadLine());
-
-        Console.Write("Enter Address: ");
-        contact.SetAddress(Console.ReadLine());
-
-        Console.Write("Enter City: ");
-        contact.SetCity(Console.ReadLine());
-
-        Console.Write("Enter State: ");
-        contact.SetState(Console.ReadLine());
-
-        Console.Write("Enter Zip: ");
-        contact.SetZip(Console.ReadLine());
-
-        Console.Write("Enter Phone: ");
-        contact.SetPhone(Console.ReadLine());
-
-        Console.Write("Enter Email: ");
-        contact.SetEmail(Console.ReadLine());
-
-        isContactAdded = true;
-        Console.WriteLine("Contact Added Successfully ");
+        AddMultipleContacts();
     }
 
-    // UC-3 : Edit Contact by Name
-    public void EditContactByName()
+    public void DisplayContact()
     {
-        if (!isContactAdded)
+        DisplayContacts();
+    }
+
+  
+    public void AddMultipleContacts()
+    {
+        Console.Write("How many contacts you want to add: ");
+        int n = int.Parse(Console.ReadLine() ?? "0");
+
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine("No contact available to edit");
+            if (count >= contacts.Length)
+            {
+                Console.WriteLine("Address Book is Full");
+                return;
+            }
+
+            Contact contact = new Contact();
+
+            Console.Write("Enter First Name: ");
+            contact.SetFirstName(Console.ReadLine());
+
+            Console.Write("Enter Last Name: ");
+            contact.SetLastName(Console.ReadLine());
+
+            Console.Write("Enter Address: ");
+            contact.SetAddress(Console.ReadLine());
+
+            Console.Write("Enter City: ");
+            contact.SetCity(Console.ReadLine());
+
+            Console.Write("Enter State: ");
+            contact.SetState(Console.ReadLine());
+
+            Console.Write("Enter Zip: ");
+            contact.SetZip(Console.ReadLine());
+
+            Console.Write("Enter Phone: ");
+            contact.SetPhone(Console.ReadLine());
+
+            Console.Write("Enter Email: ");
+            contact.SetEmail(Console.ReadLine());
+
+            contacts[count] = contact;
+            count++;
+
+            Console.WriteLine("Contact Added Successfully\n");
+        }
+    }
+
+    public void DisplayContacts()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("No contacts available");
             return;
         }
 
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine("Contact " + (i + 1));
+            Console.WriteLine(contacts[i].ToString());
+            Console.WriteLine();
+        }
+    }
+
+    public void EditContactByName()
+    {
         Console.Write("Enter First Name to Edit: ");
         string firstName = Console.ReadLine();
 
         Console.Write("Enter Last Name to Edit: ");
         string lastName = Console.ReadLine();
 
-        if (contact.GetFirstName() == firstName &&
-            contact.GetLastName() == lastName)
+        for (int i = 0; i < count; i++)
         {
-            Console.Write("Enter New Address: ");
-            contact.SetAddress(Console.ReadLine());
+            if (contacts[i].GetFirstName() == firstName &&
+                contacts[i].GetLastName() == lastName)
+            {
+                Console.Write("Enter New Address: ");
+                contacts[i].SetAddress(Console.ReadLine());
 
-            Console.Write("Enter New City: ");
-            contact.SetCity(Console.ReadLine());
+                Console.Write("Enter New City: ");
+                contacts[i].SetCity(Console.ReadLine());
 
-            Console.Write("Enter New State: ");
-            contact.SetState(Console.ReadLine());
+                Console.Write("Enter New State: ");
+                contacts[i].SetState(Console.ReadLine());
 
-            Console.Write("Enter New Zip: ");
-            contact.SetZip(Console.ReadLine());
+                Console.Write("Enter New Zip: ");
+                contacts[i].SetZip(Console.ReadLine());
 
-            Console.Write("Enter New Phone: ");
-            contact.SetPhone(Console.ReadLine());
+                Console.Write("Enter New Phone: ");
+                contacts[i].SetPhone(Console.ReadLine());
 
-            Console.Write("Enter New Email: ");
-            contact.SetEmail(Console.ReadLine());
+                Console.Write("Enter New Email: ");
+                contacts[i].SetEmail(Console.ReadLine());
 
-            Console.WriteLine("Contact updated successfully ");
-        }
-        else
-        {
-            Console.WriteLine("Contact not found ");
-        }
-    }
-
-    // UC-4 : Delete Contact by Name
-    public void DeleteContactByName()
-    {
-        if (!isContactAdded)
-        {
-            Console.WriteLine("No contact available to delete");
-            return;
+                Console.WriteLine("Contact Updated Successfully");
+                return;
+            }
         }
 
-        Console.Write("Enter First Name to Delete: ");
-        string firstName = Console.ReadLine();
-
-        Console.Write("Enter Last Name to Delete: ");
-        string lastName = Console.ReadLine();
-
-        if (contact.GetFirstName() == firstName &&
-            contact.GetLastName() == lastName)
-        {
-            contact = new Contact();   
-            isContactAdded = false;
-
-            Console.WriteLine("Contact deleted successfully ");
-        }
-        else
-        {
-            Console.WriteLine("Contact not found");
-        }
-    }
-
-    public void DisplayContact()
-    {
-        if (!isContactAdded)
-        {
-            Console.WriteLine("No contact to display");
-            return;
-        }
-
-        Console.WriteLine("\nContact Details:");
-        Console.WriteLine(contact.ToString());
+        Console.WriteLine("Contact Not Found");
     }
 }
