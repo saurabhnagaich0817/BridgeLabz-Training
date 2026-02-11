b648efe1f9c8c063b7ad000c81fe936a0fb53c31
saurabhnagaich0817 <saurabhnagaich27@gmail.com>
2026-02-11 14:47:29 +0530

    [Saurabh Nagayach] added healthcare application

---
+++ dbms-csharp-practice/scenario-based/HealthClinicConsoleApp/Exceptions/DeleteNotAllowedException.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace Exceptions
+{
+    public class DeleteNotAllowedException : System.Exception
+    {
+        public DeleteNotAllowedException()
+            : base("Delete operation cannot be performed due to dependent records.")
+        {
+        }
+
+        public DeleteNotAllowedException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ dbms-csharp-practice/scenario-based/HealthClinicConsoleApp/Main/Program.cs
@@ -0,0 +1,33 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+using Menu;
+
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = "Healthcare Management System";
+
+                MainMenu menu = new MainMenu();
+                menu.Show();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Application crashed!");
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using the system.");
+            }
+        }
+    }
+}
